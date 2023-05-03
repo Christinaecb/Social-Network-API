63d2c69d661c9b1168c64bfcb5870761545d30f1
Christina <christina.e.c.barberi@gmail.com>
2023-05-03 14:10:33 -0400

    fixed issue in server.js

--- models/Thought.js
+++ models/Thought.js
@@ -1,36 +1,35 @@
 const { Schema, model } = require('mongoose');
 
-const reactionSchema = new Schema(
-    {
-        reactionId: {
-            type: Schema.Types.ObjectId,
-            default: () => new Types.ObjectId(),
-        },
-        reactionBody: {
-            type: String,
-            required: true,
-            trim: true,
+const ReactionSchema = new Schema(
+	{
+		reactionId: {
+			type: Schema.Types.ObjectId,
+			default: () => new Types.ObjectId(),
+		},
+		reactionBody: {
+			type: String,
+			required: true,
+			trim: true,
 			minlength: 1,
-            maxlength: 280
-        },
-        createdAt: {
-            type: Date,
-            default: Date.now,
-            get: (createdAtVal) => dateFormat(createdAtVal),
-        },
-        username: {
-            type: String,
-            required: true,
-            trim: true,
-        },
-    },
-    {
-        toJSON: {
-          getters: true
-        },
-        id: false
-    }
-)
+			maxlength: 280,
+		},
+		username: {
+			type: String,
+			required: true,
+			trim: true,
+		},
+		createdAt: {
+			type: Date,
+			default: Date.now,
+			get: (createdAtVal) => dateFormat(createdAtVal),
+		},
+	},
+	{
+		toJSON: {
+			getters: true,
+		},
+	}
+);
 
 const ThoughtSchema = new Schema(
 	{
