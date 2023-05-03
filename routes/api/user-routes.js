31b6ea54d77ab43857c7bb15c1f4bf59a66a0eeb
Christina <christina.e.c.barberi@gmail.com>
2023-05-03 13:44:10 -0400

    updated user and thought routes

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,22 +1,29 @@
 const router = require('express').Router();
 
 const {
-    getUsers,
-    getSingleUser,
-    createUser,
-    updateUser,
-    deleteUser,
-    addFriend,
-    deleteFriend,
+    getAllUsers,
+	getUserById,
+	createUser,
+	updateUser,
+	deleteUser,
+	addFriend,
+	removeFriend,
 } = require('../../controllers/user-controllers')
 
-// api/users
-router.route('/').get(getUsers).post(createUser);
+// /api/users
+router.route("/")
+	.get(getAllUsers)
+	.post(createUser);
 
-// api/users/:userId
-router.route('/:userId').get(getSingleUser).put(updateUser).delete(deleteUser)
+// /api/users/<id>
+router.route("/:id")
+	.get(getUserById)
+	.put(updateUser)
+	.delete(deleteUser);
 
-// api/users/:userId/friends/:friendId
-router.route('/:userId/friends/:friendId').post(addFriend).delete(deleteFriend)
+// /api/users/<userId>/friends/<friendId>
+router.route("/:userId/friends/:friendId")
+	.post(addFriend)
+	.delete(removeFriend);
 
 module.exports = router;
